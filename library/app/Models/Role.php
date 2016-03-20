5ba801c8fda203ee1e69c96177d145f9f5f7d9dd
Oliver Yepez <oliver.yepez@gmail.com>
2016-03-19 18:58:06 -0600

    Add softdelete on Models

--- library/app/Models/Role.php
+++ library/app/Models/Role.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
+
+    use SoftDeletes;
 
     protected $table = 'roles';
 
@@ -13,6 +16,8 @@ class Role extends Model
         'name',
         'details'
     ];
+
+    protected $dates = ['created_at, updated_at', 'deleted_at'];
 
     public static function searchByKeyword($keyword){
 
